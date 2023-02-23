d1d84aa0c8624e8cf569b6e04089747ab517cc9e
mkwozniak <51724102+mkwozniak@users.noreply.github.com>
2023-02-23 01:02:22 -0500

    Updated Rotate2D
    Moved using directives inside namespace

--- Assets/Scripts/Rotate2D.cs
+++ Assets/Scripts/Rotate2D.cs
@@ -1,9 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace Wozware.ZSpaceDemo
 {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
     public class Rotate2D : MonoBehaviour
     {
         public float RotationSpeed;
